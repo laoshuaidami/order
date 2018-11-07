e4db2a180ccefc17422af6841fc423332e019ebd
laoshuaidami <14220283@qq.com>
2018-11-07 19:31:03 +0800

    11.7_1

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Application(Flask):
+    def __init__(self,import_name):
+        super(Application, self).__init__(import_name)
+
+
+app = Flask(__name__)
+db = SQLAlchemy()
