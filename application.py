50936eb989bc9279f81dbe3e65d462d0b3f648b6
laoshuaidami <14220283@qq.com>
2018-11-09 16:56:18 +0800

    11.9

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from flask import Flask
 from flask_script import Manager
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+
 
 class Application(Flask):
     def __init__(self,import_name,template_folder=None,root_path=None):
@@ -12,13 +13,14 @@ class Application(Flask):
 
         db.init_app(self)
 
+
 db = SQLAlchemy()
-app = Application(__name__,template_folder=os.getcwd()+"/web/templates/",root_path=os.getcwd())
+app = Application(__name__,template_folder=os.getcwd()+"/web/templates/", root_path=os.getcwd())
 manager = Manager(app)
 
 '''
 函数方法
 '''
-from common.libs.UrlManager import  UrlManager
-app.add_template_global(UrlManager.buildStaticUrl,'buildStaticUrl')
-app.add_template_global(UrlManager.buildUrl,'buildUrl')
+from common.libs.UrlManager import UrlManager
+app.add_template_global(UrlManager.buildStaticUrl, 'buildStaticUrl')
+app.add_template_global(UrlManager.buildUrl, 'buildUrl')
--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,12 +1,47 @@
-from flask import Blueprint,render_template
+# -*- coding: utf-8 -*-
+from flask import Blueprint, render_template, request, jsonify
+from common.models.User import User
+from common.libs.user.UserService import UserService
+route_user = Blueprint('user_page', __name__)
 
-route_user = Blueprint('/user_page',__name__)
-@route_user.route('/login')
+
+@route_user.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template ("user/login.html")
-@route_user.route('/edit')
+    if request.method == "GET":
+        return render_template("user/login.html")
+    resp = {'code': 200, "msg": "登录成功", 'data': {}}
+    req = request.values
+    login_name = req['login_name'] if 'login_name' in req else ''
+    login_pwd = req['login_pwd'] if 'login_pwd' in req else ''
+
+    if login_name is None or len(login_name) < 1:
+        resp['code'] = -1
+        resp["msg"] = "请输入正确的用户名"
+        return jsonify(resp)
+
+    if login_pwd is None or len(login_pwd) < 1:
+        resp['code'] = -1
+        resp['msg'] = "请输入正确的密码"
+        return jsonify(resp)
+
+    user_info = User.query.filter_by(login_name=login_name).first()
+    if not user_info:
+        resp['code'] = -1
+        resp['msg'] = "请输入正确的用户名和密码-1"
+        return jsonify(resp)
+    if user_info.login_pwd != UserService.genePwd(login_pwd,user_info.login_salt):
+        resp['code'] = -1
+        resp['msg'] = "请输入正确的用户名和密码-2"
+        return jsonify(resp)
+
+    return jsonify(resp)
+
+
+@route_user.route("/edit")
 def edit():
-    return render_template ("user/edit.html")
-@route_user.route('/reset-pwd')
+    return render_template( "user/edit.html")
+
+
+@route_user.route("/reset-pwd")
 def resetPwd():
-    return render_template ("user/reset_pwd.html")
+    return render_template("user/reset_pwd.html")
